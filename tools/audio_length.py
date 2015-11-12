fa7c4b7ae87b621043ffda1f1f00a74802db1710
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2015-11-12 13:57:38 +0100

    corrected script

--- tools/audio_length.py
+++ tools/audio_length.py
@@ -1,13 +1,16 @@
 import os
 import subprocess
+import sys
 
 
 def get_audio_length(f):
+  print f 
   command = "soxi -D %s" % f
   return float(subprocess.check_output(command, shell=True))
 
 if __name__ == '__main__':
   target_dir = sys.argv[1]
   files = os.listdir(target_dir)
-  total = sum([get_audio_length(os.path.join(target_dir, f) for f in files)])
+  print files
+  total = sum([get_audio_length(os.path.join(target_dir, f)) for f in files])
   print total / 60. / 60.
