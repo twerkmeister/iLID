a9b8bff2e5a5a343d80cef4333413d1084324c36
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2015-11-23 14:16:18 +0100

    start script for spark pipeline. Creating package files and submitting to spark in one script

--- preprocessing/sparkline.py
+++ preprocessing/sparkline.py
@@ -16,14 +16,15 @@ def apply_melfilter(f, samplerate, signal):
 def main(args):
   files = filecollector.collect(args.input_path)
 
-  sc = SparkContext("local", "sparkline", )
+  sc = SparkContext("local", "sparkline")
   pipeline = (
     sc.parallelize(files)
     .map(lambda f: read_wav(f))
     .map(lambda (f, samplerate_and_signal): apply_melfilter(f, samplerate_and_signal[0], samplerate_and_signal[1]))
     .map(lambda (f, filterbank_energies): (f, graphic.colormapping.to_rgb(filterbank_energies, bytes=True)))
     .map(lambda (f, image): (f, list(graphic.windowing.sliding(image, 600, 600))))
-    .map(lambda (f, images): output.image.save(f, images, args.output_path)))
+    .map(lambda (f, images): output.image.save(f, images, args.output_path))
+  )
   
   pipeline.collect()
 
