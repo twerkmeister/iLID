ca74380ef9f8f4b4468af9ce82e13ca005ae0aae
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2015-11-25 22:38:23 +0100

    parameters

--- preprocessing/sparkline.py
+++ preprocessing/sparkline.py
@@ -10,7 +10,7 @@ def read_wav(f):
   return (f, wav.read(f))
 
 def apply_melfilter(f, samplerate, signal):
-  filterbank_energies = audio.melfilterbank.logfilter(samplerate, signal, winlen=0.00833, winstep=0.00833, nfilt=39, lowfreq=0)
+  filterbank_energies = audio.melfilterbank.logfilter(samplerate, signal, winlen=0.00833, winstep=0.00833, nfilt=39, lowfreq=0, preemph=1.0)
   return (f, filterbank_energies)
 
 def main(args):
@@ -22,7 +22,7 @@ def main(args):
     .map(lambda f: read_wav(f))
     .map(lambda (f, samplerate_and_signal): apply_melfilter(f, samplerate_and_signal[0], samplerate_and_signal[1]))
     .map(lambda (f, filterbank_energies): (f, graphic.colormapping.to_grayscale(filterbank_energies, bytes=True)))
-    .map(lambda (f, image): (f, list(graphic.windowing.sliding(image, 600, 600))))
+    .map(lambda (f, image): (f, list(graphic.windowing.sliding(image, 600, 600, 0.6))))
     .map(lambda (f, images): output.image.save(f, images, args.output_path))
   )
   
