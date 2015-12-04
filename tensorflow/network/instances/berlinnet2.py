f26179d7cec6bfd120da72ee43e115adc304b4ac
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2015-12-04 17:03:22 +0100

    added berlinnet2 with one more fully connected layer

---
+++ tensorflow/network/instances/berlinnet2.py
@@ -0,0 +1,19 @@
+from network import *
+import tensorflow as tf
+
+labels = 2
+berlin_net_2= Network("Berlin",
+                      [39, 600, 1],
+                      [labels],
+                      [ConvolutionLayer(6, 6, 1, 1, 12),
+                       LocalResponseNormalizationLayer(),
+                       PoolingLayer(2, 2, 2, 2),
+                       ConvolutionLayer(6, 6, 1, 1, 12),
+                       LocalResponseNormalizationLayer(),
+                       PoolingLayer(2, 2, 2, 2),
+                       ConvolutionLayer(6, 6, 1, 1, 12),
+                       LocalResponseNormalizationLayer(),
+                       PoolingLayer(2, 2, 2, 2),
+                       FullyConnectedLayer(2048),
+                       FullyConnectedLayer(1024),
+                       FullyConnectedLayer(labels, activation_function=tf.identity)])
