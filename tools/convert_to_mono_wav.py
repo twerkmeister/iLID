09a8d22aa4111afcc706a2370a65229dbc6b0b17
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2015-12-08 10:59:13 +0100

    converting to wav and mono at the same time

--- tools/convert_to_mono_wav.py
+++ tools/convert_to_mono_wav.py
@@ -9,7 +9,7 @@ def convert(filename):
   filename_extensionless, extension = os.path.splitext(filename)
   new_filename = "".join([filename_extensionless, ".wav"])
   if not os.path.exists(new_filename):
-    command = "ffmpeg -i \"{}\" \"{}\"".format(escape_characters(filename), escape_characters(new_filename))
+    command = "ffmpeg -i \"{}\" -ac 1 \"{}\"".format(escape_characters(filename), escape_characters(new_filename))
     subprocess.call(command, shell=True)
 
 
@@ -17,6 +17,7 @@ def walk_path(path):
   for root, dirs, files in os.walk(path):
     for sound_file in files:
       _, extension = os.path.splitext(sound_file)
+      print sound_file
       if extension in filetypes_to_convert:
         yield os.path.join(root, sound_file)
       else:
