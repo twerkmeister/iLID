85081d36d4d3e1278f05484345f48280d3e9c636
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2015-12-08 11:24:45 +0100

    converting webm, audio length only for wav files

--- tools/audio_length.py
+++ tools/audio_length.py
@@ -15,5 +15,6 @@ def get_audio_length(f):
 if __name__ == '__main__':
   target_dir = sys.argv[1]
   files = os.listdir(target_dir)
+  files = filter(lambda f: os.path.splitext(f)[1] == ".wav", files)
   total = sum([get_audio_length(os.path.join(target_dir, f)) for f in files])
   print total / 60. / 60.
