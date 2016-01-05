7382a3df0198d9d48ed1b9d84b53af23299f0ec4
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2016-01-05 13:30:45 +0100

    simple prefetching

--- tensorflow/networkinput/networkinput.py
+++ tensorflow/networkinput/networkinput.py
@@ -7,6 +7,9 @@ class NetworkInput(object):
         self.batch_start = 0
         self.epochs_completed = 0
         self.input_shape = input_shape
+        self.cache = np.array([])
+        self.cache_iterator = 0
+        self.cache_factor = 10
 
     def next_batch(self, batch_size):
         raise NotImplemented
@@ -15,3 +18,16 @@ class NetworkInput(object):
         v = np.zeros(self.num_labels)
         v[label] = 1
         return v
+
+    def next_batch_cached(self, batch_size):
+        print batch_size
+        if self.cache_iterator == 0:
+            self.cache = self.next_batch(batch_size * self.cache_factor)
+        
+        result_images = self.cache[0][self.cache_iterator * batch_size : (self.cache_iterator+1) * batch_size]
+        result_labels = self.cache[1][self.cache_iterator * batch_size : (self.cache_iterator+1) * batch_size]
+        self.cache_iterator = (self.cache_iterator + 1) % self.cache_factor
+        print result_images.shape
+        return result_images, result_labels
+
+
