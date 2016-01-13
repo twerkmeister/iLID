1cf60fd5c4e5ff67ea94ffd9d4ee85f20d7450fa
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2016-01-13 12:41:14 +0100

    initializing last layers

--- tensorflow/network/instances/berlinnet_unnormal.py
+++ tensorflow/network/instances/berlinnet_unnormal.py
@@ -13,4 +13,6 @@ def generate(input_shape, num_labels):
                     ConvolutionLayer(6, 6, 1, 1, 12),
                     PoolingLayer(2, 2, 2, 2),
                     FullyConnectedLayer(1024),
-                    FullyConnectedLayer(num_labels, activation_function=tf.identity)])
+                    FullyConnectedLayer(num_labels, activation_function=tf.identity,
+                      weights_initializer=tf.truncated_normal_initializer(stddev=1/float(1024)), 
+                      bias_initializer=tf.constant_initializer(np.log(1/float(num_labels))))])
