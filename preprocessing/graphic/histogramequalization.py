2dc74627241e866981e1434ab97388b399346796
Tom Herold <tom.herold@scalableminds.com>
2016-01-26 15:27:10 +0100

    added levels filter

--- preprocessing/graphic/histogramequalization.py
+++ preprocessing/graphic/histogramequalization.py
@@ -11,3 +11,34 @@ def histeq(image, number_bins=256):
    normalized_image = np.interp(image.flatten(), bins[:-1], cdf)
 
    return normalized_image.reshape(image.shape)
+
+def clamp_and_equalize(image):
+   # Clamp and normalize the remaining histogram
+   # Similar to Levels Filter in Photoshop etc
+
+   min_val = 100 # clamp ca. 40% of black values
+   max_val = image.max()
+
+   LUT = np.zeros((256,)) # Look up table
+
+   # clamp all values smaller/greater than min/max
+   LUT[:min_val] = 0
+   LUT[max_val:] = 255
+
+   # re-distribute all values between min and max
+   factor = (255.0 / (max_val - min_val))
+   base = 0
+
+   for i in range(min_val, max_val):
+    LUT[i] = base
+    base += factor
+
+   # apply the Lookup Table
+   height, width = image.shape
+
+   for i in np.arange(height):
+    for j in np.arange(width):
+      image[i][j] = LUT[image[i][j]]
+
+   # return np.take(LUT, image)
+   return image
