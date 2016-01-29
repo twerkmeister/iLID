37d22d99f8da5b786b06ff5c4af6c5a77310afa1
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2016-01-29 16:00:32 +0100

    moved truncate into function

--- preprocessing/sparkline.py
+++ preprocessing/sparkline.py
@@ -11,7 +11,8 @@ def read_wav(f):
   samplerate, signal = wav.read(f)
   #if len(signal.shape) > 1:
   #  signal = signal[:,0]
-  return (filename.clean(f), signal, samplerate)
+  f = filename.truncate_extension(filename.clean(f))
+  return (f, signal, samplerate)
 
 def apply_melfilter(f, signal, samplerate):
   filterbank_energies = audio.melfilterbank.logfilter(samplerate, signal, winlen=0.00833, winstep=0.00833, nfilt=39, lowfreq=0, preemph=1.0)
@@ -39,20 +40,18 @@ def main(args):
   pipeline = (
     sc.parallelize(files, 4)
     .map(lambda f: read_wav(f))
-    .map(lambda (f, signal, samplerate): (filename.truncate_extension(f), signal, samplerate))
     .flatMap(lambda (f, signal, samplerate): sliding_audio(f, signal, samplerate))
     .map(lambda (f, signal, samplerate): downsample(f, signal, samplerate))
-    .map(lambda (f, signal, samplerate): generate_spectrograms(f, signal, samplerate))
-    #.map(lambda (f, signal, samplerate): apply_melfilter(f, signal, samplerate))
+    .map(lambda (f, signal, samplerate): apply_melfilter(f, signal, samplerate))
     .map(lambda (f, image): (f, graphic.colormapping.to_grayscale(image, bytes=True)))
     .map(lambda (f, image): (f, graphic.histeq.histeq(image)))
     .map(lambda (f, image): (f, graphic.windowing.cut_or_pad_window(image, window_size)))
     .map(lambda (f, image): output.image.save(f, image, args.output_path))
   )
-  
+
   pipeline.collect()
 
-
+#.map(lambda (f, signal, samplerate): generate_spectrograms(f, signal, samplerate))
 if __name__ == '__main__':
 
   args = argparser.parse()
